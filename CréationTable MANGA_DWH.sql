66fd4e59aa04f1616766b022ddc61cda5f31e707
MatteoDelin <matteo.delin@univ-lyon2.fr>
2026-02-09 17:23:46 +0100

    /add dernière tranfo

--- CréationTable MANGA_DWH.sql
+++ CréationTable MANGA_DWH.sql
@@ -19,8 +19,41 @@ CREATE TABLE IF NOT EXISTS dim_demographic (
   demographic_nom     VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS DIM_date (
-    date VARCHAR(50) PRIMARY KEY
+CREATE TABLE dim_manga (
+    manga_id INT PRIMARY KEY,
+    manga_title VARCHAR(255),
+    manga_title_en VARCHAR(255),
+    manga_published_from VARCHAR(50),
+    manga_published_to VARCHAR(50)
 );
 
-select * from dim_date
+CREATE TABLE manga_genre (
+    manga_id INT,
+    genre_id INT,
+    PRIMARY KEY (manga_id, genre_id),
+    FOREIGN KEY (manga_id) REFERENCES dim_manga(manga_id),
+    FOREIGN KEY (genre_id) REFERENCES dim_genre(genre_id)
+);
+
+CREATE TABLE FACT_Manga_Sales (
+    manga_id INT NOT NULL,
+    publisher_id INT NOT NULL,
+    demographic_id INT NOT NULL,
+    author_id INT NOT NULL,
+    no_of_collected_volumes INT,
+    manga_chapters INT,
+    score DOUBLE PRECISION,
+    average_sales_per_volume_in_million DOUBLE PRECISION,
+    approximate_sales_in_million DOUBLE PRECISION,
+
+    PRIMARY KEY (manga_id, publisher_id, demographic_id, author_id),
+    FOREIGN KEY (manga_id) REFERENCES dim_manga(manga_id),
+    FOREIGN KEY (publisher_id) REFERENCES dim_publisher(publisher_id),
+    FOREIGN KEY (demographic_id) REFERENCES dim_demographic(demographic_id),
+    FOREIGN KEY (author_id) REFERENCES dim_author(author_id)
+);
+
+
+select * from fact_manga_sales;
+
+
